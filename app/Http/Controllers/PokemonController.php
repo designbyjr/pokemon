18d89dad3f1d17f6ae8138aa8cc7464099473c4f
Jamie Design BY JR <one4allplugin@gmail.com>
2024-07-14 15:36:10 +0100

    updated routes to comply with open api requirements

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -5,34 +5,222 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Helpers\PokemonHelper;
+
+/**
+ * @OA\Info(title="Pokemon API", version="1.0")
+ */
 class PokemonController extends Controller
 {
-    public function getPokemon($name)
+    /**
+     * @OA\Get(
+     *     path="/pokemon",
+     *     operationId="index",
+     *     tags={"pokemon"},
+     *     summary="Get list of all Pokemon",
+     *     description="Returns a list of all Pokemon",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(type="array", @OA\Items(type="object"))
+     *     )
+     * )
+     */
+    public function index()
     {
-        $response = Http::get("https://pokeapi.co/api/v2/pokemon/{$name}");
-
-        if ($response->successful()) {
-            $data = $response->json();
-            $helper = new PokemonHelper();
-            $response = $helper->TransposeSearchResponse($data);
-            return response()->json($response);
-        } else {
-            return response()->json(['error' => 'Pokemon not found'], 404);
-        }
-    }
-
-    public function getDefaultPokemon()
-    {
-        // Example: Fetch default Pokémon, e.g., "pikachu"
         $response = Http::get("https://pokeapi.co/api/v2/pokemon/?limit=1350");
 
         if ($response->successful()) {
             $data = $response->json();
             $helper = new PokemonHelper();
-            $response = $helper->TransposeDefaultResponse($data['results']);
-            return response()->json($response);
+            $transposedResponse = $helper->transposeDefaultResponse($data['results']);
+            return response()->json($transposedResponse);
         } else {
             return response()->json(['error' => 'Default Pokemon not found'], 404);
         }
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/pokemon/{name}",
+     *     operationId="show",
+     *     tags={"pokemon"},
+     *     summary="Get Pokemon by name",
+     *     description="Returns Pokemon details by name",
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="path",
+     *         description="Name of the Pokemon",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pokemon not found",
+     *         @OA\JsonContent(type="object", @OA\Property(property="error", type="string"))
+     *     )
+     * )
+     */
+    public function show($id)
+    {
+        $response = Http::get("https://pokeapi.co/api/v2/pokemon/{$id}");
+
+        if ($response->successful()) {
+            $data = $response->json();
+            $helper = new PokemonHelper();
+            $transposedResponse = $helper->transposeOverviewResponse($data);
+            return response()->json($transposedResponse);
+        } else {
+            return response()->json(['error' => 'Pokemon not found'], 404);
+        }
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/pokemon/search",
+     *     operationId="search",
+     *     tags={"pokemon"},
+     *     summary="Search Pokemon",
+     *     description="Search Pokemon by name",
+     *     @OA\Parameter(
+     *         name="query",
+     *         in="query",
+     *         description="Search query",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(type="array", @OA\Items(type="object"))
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pokemon not found",
+     *         @OA\JsonContent(type="object", @OA\Property(property="error", type="string"))
+     *     )
+     * )
+     */
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $response = Http::get("https://pokeapi.co/api/v2/pokemon/{$query}");
+
+        if ($response->successful()) {
+            $data = $response->json();
+            $helper = new PokemonHelper();
+            $transposedResponse = $helper->transposeSearchResponse($data);
+            return response()->json($transposedResponse);
+        } else {
+            return response()->json(['error' => 'Pokemon not found'], 404);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/pokemon",
+     *     operationId="store",
+     *     tags={"pokemon"},
+     *     summary="Store a new Pokemon",
+     *     description="Creates a new Pokemon entry",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Pokemon created successfully",
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid input",
+     *         @OA\JsonContent(type="object", @OA\Property(property="error", type="string"))
+     *     )
+     * )
+     */
+
+
+    public function store(Request $request)
+    {
+        // Implementation for storing a new Pokemon entry
+        return response()->json(['message' => 'Pokemon created successfully'], 201);
+    }
+
+    /**
+     * @OA\Put(
+     *     path="/pokemon/{name}",
+     *     operationId="update",
+     *     tags={"pokemon"},
+     *     summary="Update an existing Pokemon",
+     *     description="Updates an existing Pokemon entry",
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="path",
+     *         description="Name of the Pokemon",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Pokemon updated successfully",
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid input",
+     *         @OA\JsonContent(type="object", @OA\Property(property="error", type="string"))
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pokemon not found",
+     *         @OA\JsonContent(type="object", @OA\Property(property="error", type="string"))
+     *     )
+     * )
+     */
+    public function update(Request $request, $name)
+    {
+        // Implementation for updating an existing Pokemon entry
+        return response()->json(['message' => 'Pokemon updated successfully']);
+    }
+
+    /**
+     * @OA\Delete(
+     *     path="/pokemon/{name}",
+     *     operationId="destroy",
+     *     tags={"pokemon"},
+     *     summary="Delete a Pokemon",
+     *     description="Deletes a Pokemon by name",
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="path",
+     *         description="Name of the Pokemon",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Pokemon deleted successfully",
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pokemon not found",
+     *         @OA\JsonContent(type="object", @OA\Property(property="error", type="string"))
+     *     )
+     * )
+     */
+    public function destroy($name)
+    {
+        // Implementation for deleting a Pokemon entry
+        return response()->json(['message' => 'Pokemon deleted successfully']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonController;
 
-Route::get('/', function(){return view('welcome');});
+Route::get('/', function(){return view('index');});
 
-Route::post('/pokemon/search/{name}', [PokemonController::class, 'getPokemon'])->name('getPokemon');
-Route::get('/pokemon/fetch/{id}', [PokemonController::class, 'getPokemonOverview'])->name('pokemonOverview');
-Route::get('/pokemon/default', [PokemonController::class, 'getDefaultPokemon'])->name('defaultPokemon');
+Route::post('/pokemon/search/{name}', [PokemonController::class, 'search'])->name('getPokemon');
+Route::get('/pokemon/show/{id}', [PokemonController::class, 'show'])->name('pokemonOverview');
+Route::get('/pokemon/index', [PokemonController::class, 'index'])->name('defaultPokemon');
