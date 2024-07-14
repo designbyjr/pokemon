92509f57e93549a26088f8b57b6862523bb83c1e
Jamie Design BY JR <one4allplugin@gmail.com>
2024-07-14 15:50:27 +0100

    updated routes to comply forcing json only

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonController;
+use App\Http\Middleware\ForceJsonResponse;
 
 Route::get('/', function(){return view('index');});
 
-Route::post('/pokemon/search/{name}', [PokemonController::class, 'search'])->name('getPokemon');
-Route::get('/pokemon/show/{id}', [PokemonController::class, 'show'])->name('pokemonOverview');
-Route::get('/pokemon/index', [PokemonController::class, 'index'])->name('defaultPokemon');
+Route::group(['middleware' => ForceJsonResponse::class], function () {
+    Route::post('/pokemon/search/{name}', [PokemonController::class, 'search'])->name('getPokemon');
+    Route::get('/pokemon/show/{id}', [PokemonController::class, 'show'])->name('pokemonOverview');
+    Route::get('/pokemon/index', [PokemonController::class, 'index'])->name('defaultPokemon');
+});
